3d21ec78e9f32b535ebd42e65b555f32d162000c
narcosm <narcosm@correo.udistrital.edu.co>
2021-10-19 12:02:04 -0500

    Cliente estadistica Jose David

--- client/index.js
+++ client/index.js
@@ -1,9 +1,11 @@
-const usersClient = require ('./usersClient')
+const usersClient = require('./usersClient')
 const filesClient = require('./filesClient')
 const publicarClient = require('./publicarClient')
+const stadisticsClient = require('./stadisticsClient')
 
 module.exports = {
     usersClient,
     filesClient,
-    publicarClient
+    publicarClient,
+    stadisticsClient
 }
